2949efda677a28c18f5cb313015e8cd9672a300f
BrandanM5321 <brandonmichelle2013@icloud.com>
2017-10-10 14:32:30 -0500

    Complete Complete
    Give 100% give 100%

---
+++ CreatureWorld.java
@@ -0,0 +1,79 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import java.util.List;
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (Brandan Micheelle) 
+ * @version (a version number or a date)
+ */
+public class CreatureWorld extends World
+{
+    private Creature playerOneCreature;
+    private Creature playerTwoCreature;
+    /**
+     * Default constructor for objects of class MyWorld.
+     * @author (Brandan Michelle)
+     * @param There are no parameters
+     * @return an object of class MyWorld
+     */
+    public CreatureWorld()
+    {
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(400, 400, 1);
+        playerOneCreature = new Charmander(this);
+        playerTwoCreature = new Pikachu (this);
+        
+        
+        prepareCreature();
+        Greenfoot.start();
+    }
+    
+    private void prepareCreature()
+    {
+        addObject( playerOneCreature, playerOneCreature.getImage().getWidth()/2, getHeight() - playerOneCreature.getImage().getHeight()/2);
+        addObject( new Button(Color.RED, 50), getWidth()/2, getHeight() - 100 );
+        addObject( playerTwoCreature, getWidth() - playerTwoCreature.getImage().getWidth()/2, playerTwoCreature.getImage().getHeight()/2);
+        addObject( new Button( Color.RED, 50), getWidth()/2, 100);
+    }
+    
+    public Creature getPlayerOne()
+    {
+        return playerOneCreature;
+    }
+    
+    public Creature getPlayerTwo()
+    {
+        return playerTwoCreature;
+    }
+    
+    /**
+     * act will complete actions that the CreatureWorld object should
+     * accomplish while the scenario is running
+     * @author (Brandan Michelle)
+     * @param There are no parameters
+     * @return Nothing is returned
+     */
+    public void act()
+    {
+       List allObjects = getObjects(null);
+       if ( playerTwoCreature. getHealthBar().getCurrent() <=0 )
+       {
+          removeObjects(allObjects);
+          showText("Player Two Wins",getWidth()/2, getHeight ()/2 );
+          Greenfoot.stop();
+           
+       }
+       
+       if( playerOneCreature. getHealthBar().getCurrent() <=0 )
+       {
+          removeObjects(allObjects);
+          showText("Player One Wins",getWidth()/2,getHeight()/2 );
+          Greenfoot.stop();
+          
+       }
+
+    }
+}
